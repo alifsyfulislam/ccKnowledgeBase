04a2779be0d9a2e45ad3ef09741d6d26066849fc
Noman <noman@genusys.us>
2020-11-15 15:28:47 +0600

    cors middleware updated

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,10 +13,13 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    public function handle($request, Closure $next) {
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin' , '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
+        return $response;
     }
+
 }
