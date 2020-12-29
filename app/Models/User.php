20fc40345ac91ac9a106137e6828e6081e05c4a3
Noman <noman@genusys.us>
2020-12-29 18:37:46 +0600

    time format updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
 
     public function getCreatedAtAttribute($date)
     {
-        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format("F j, Y, g:i a");
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format("j M, Y");
     }
 
     public function getUpdatedAtAttribute($date)
