b63c8ee6b3131c6a8f5930b377990dceaceb10d1
alif <alifsyfulislam@gmail.com>
2021-02-09 18:27:45 +0600

    download resources in article view

--- kbs2.1/public/back/src/components/article/articleDetails.vue
+++ kbs2.1/public/back/src/components/article/articleDetails.vue
@@ -22,68 +22,78 @@
 
         <div class="content-wrapper bg-white" v-if="aArticle">
           <div class="col-md-12 article-lang-switcher">
-              <ul class="nav nav-tabs" id="myTab" v-if="aArticle.bn_title">
-                  <li class="nav-item">
-                      <a class="nav-link active" data-toggle="tab" href="#tabEnglish">English</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#tabBangla">Bangla</a>
-                  </li>
-              </ul>
+            <ul class="nav nav-tabs" id="myTab" v-if="aArticle.bn_title">
+              <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#tabEnglish">English</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tabBangla">Bangla</a>
+              </li>
+            </ul>
           </div>
-          
+
           <div class="col-md-9 col-md-8">
             <div class="tab-content pt-3" id="myTabContent">
               <div class="tab-pane fade active show" id="tabEnglish">
                 <h1 class="mb-0 font-weight-bold">Articles Details Page</h1>
                 <div>
-                    <small class="font-16"><strong>Category: </strong>{{aArticle.category ? aArticle.category.name : 'N/A'}}</small>
+                  <small class="font-16"><strong>Category: </strong>{{aArticle.category ? aArticle.category.name : 'N/A'}}</small>
                 </div>
                 <p class="font-16"><strong>Tags: </strong>{{aArticle.tag}}</p>
                 <div class="ta-wrapper d-flex align-items-center py-10 my-40">
-                    <div class="avatar mr-10">
-                        <img class="img-fluid" src="../../assets/img/avatar.png" style="height: 50px; width: 50px" alt="avatar">
-                    </div>
-                    <div class="tc-wrapper">
-                        <h5 v-cloak class="my-0 pb-1">{{aArticle.user.first_name}} {{aArticle.user.last_name}}</h5>
-                        <p class="mb-0">Post on: {{aArticle.created_at}}</p>
-                    </div>
+                  <div class="avatar mr-10">
+                    <img class="img-fluid" src="../../assets/img/avatar.png" style="height: 50px; width: 50px" alt="avatar">
+                  </div>
+                  <div class="tc-wrapper">
+                    <h5 v-cloak class="my-0 pb-1">{{aArticle.user.first_name}} {{aArticle.user.last_name}}</h5>
+                    <p class="mb-0">Post on: {{aArticle.created_at}}</p>
+                  </div>
                 </div>
 
                 <div class="ta-content-wrapper">
-                    <h3 class="">{{aArticle.en_title}}</h3>
+                  <h3 class="">{{aArticle.en_title}}</h3>
                 </div>
                 <div class="ta-content-wrapper">
-                    <div v-html="aArticle.en_body"></div>
+                  <div v-html="aArticle.en_body"></div>
                 </div>
               </div>
 
 
-                  <!--bangla-->
+              <!--bangla-->
               <div class="tab-pane fade" id="tabBangla" v-if="aArticle.bn_title">
                 <h1 class="mb-0 font-weight-bold">Articles Details Page</h1>
                 <div>
-                    <small class="font-16"><strong>Category: </strong>{{aArticle.category ? aArticle.category.name : 'N/A'}}</small>
+                  <small class="font-16"><strong>Category: </strong>{{aArticle.category ? aArticle.category.name : 'N/A'}}</small>
                 </div>
                 <p class="font-16"><strong>Tags: </strong>{{aArticle.tag}}</p>
                 <div class="ta-wrapper d-flex align-items-center py-10 my-40">
-                    <div class="avatar mr-10">
-                        <img class="img-fluid" src="../../assets/img/avatar.png" style="height: 50px; width: 50px" alt="avatar">
-                    </div>
-                    <div class="tc-wrapper">
-                        <h5 v-cloak class="my-0 pb-1">{{aArticle.user.first_name}} {{aArticle.user.last_name}}</h5>
-                        <p class="mb-0">Post on: {{aArticle.created_at}}</p>
-                    </div>
+                  <div class="avatar mr-10">
+                    <img class="img-fluid" src="../../assets/img/avatar.png" style="height: 50px; width: 50px" alt="avatar">
+                  </div>
+                  <div class="tc-wrapper">
+                    <h5 v-cloak class="my-0 pb-1">{{aArticle.user.first_name}} {{aArticle.user.last_name}}</h5>
+                    <p class="mb-0">Post on: {{aArticle.created_at}}</p>
+                  </div>
                 </div>
 
                 <div class="ta-content-wrapper">
-                      <h3 class="">{{aArticle.bn_title}}</h3>
+                  <h3 class="">{{aArticle.bn_title}}</h3>
                 </div>
                 <div class="ta-content-wrapper">
-                    <div v-html="aArticle.bn_body"></div>
+                  <div v-html="aArticle.bn_body"></div>
                 </div>
               </div>
             </div>
+
+          </div>
+
+          <div class="col-md-12">
+            <h5 class="mb-0 font-weight-bold pb-2">Download Resources</h5>
+            <ul class="pl-15">
+              <li v-for="a_file in aArticle.media" :key="a_file.id">
+                <a :href="a_file.url">{{a_file.url | formatFileName }}</a>
+              </li>
+            </ul>
           </div>
 
           <!-- Content Area End -->
@@ -101,27 +111,34 @@ import Header from "@/layouts/common/Header";
 import Menu from "@/layouts/common/Menu";
 import axios from "axios";
 
-  export default {
-    name: "articleDetails.vue",
-    components: {
-      Header,
-      Menu
-    },
-    data(){
+export default {
+  name: "articleDetails.vue",
+  components: {
+    Header,
+    Menu
+  },
+  filters:{
+    formatFileName(fileName){
+      let res;
+      res = fileName.split('/')
+      return res[res.length - 1];;
+    }
+  },
+  data(){
 
-      return{
+    return{
 
-        articleID: '',
-        aArticle: '',
-        articleCounter: [],
-        allCategoryArticle: '',
-        categoryHasArticle: [],
-        allArticle: '',
-        query_string: ''
+      articleID: '',
+      aArticle: '',
+      articleCounter: [],
+      allCategoryArticle: '',
+      categoryHasArticle: [],
+      allArticle: '',
+      query_string: ''
 
-      }
+    }
 
-    },
+  },
 
   methods: {
 
@@ -129,11 +146,11 @@ import axios from "axios";
       let _that = this;
       let articleID = v;
       axios.get('article-details/'+articleID)
-          .then(function (response) {
-            _that.aArticle = response.data.article_info;
+        .then(function (response) {
+          _that.aArticle = response.data.article_info;
 
-            console.log(_that.aArticle);
-          })
+          console.log(_that.aArticle.media);
+        })
     },
 
     searchData(){
@@ -144,26 +161,26 @@ import axios from "axios";
     getRecentArticleList(){
       let _that = this;
       axios.get('article-list')
-          .then(function (response) {
-            _that.allArticle = response.data.article_list;
-          })
+        .then(function (response) {
+          _that.allArticle = response.data.article_list;
+        })
     },
 
     getCategoryArticleList()
     {
       let _that =this;
-     // let articleCounter = 0;
+      // let articleCounter = 0;
       axios.get('category-article-list', { cache: false })
-          .then(function (response) {
-            if(response.data.status_code === 200){
-              _that.allCategoryArticle = response.data.category_list;
-              _that.allCategoryArticle.forEach(val =>{
-                if (val.article.length!=0){
-                  _that.categoryHasArticle.push(val);
-                }
-              })
-            }
-          })
+        .then(function (response) {
+          if(response.data.status_code === 200){
+            _that.allCategoryArticle = response.data.category_list;
+            _that.allCategoryArticle.forEach(val =>{
+              if (val.article.length!=0){
+                _that.categoryHasArticle.push(val);
+              }
+            })
+          }
+        })
     },
 
   },
