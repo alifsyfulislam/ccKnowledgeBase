75f603e1bdb067653014bb043a698418a2494fb2
zia <ziaur@genusys.us>
2021-02-23 10:55:56 -0800

    migration update

---
+++ kbs2.1/database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,87 @@
+<?php
+namespace Database\Seeders;
+
+use App\Helpers\Helper;
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        Permission::truncate();
+
+        $Permissions = [
+            'User List',
+            'User Create',
+            'User Edit',
+            'User Delete',
+            'Role List',
+            'Role Create',
+            'Role Edit',
+            'Role Delete',
+            'Permission List',
+            'Permission Create',
+            'Permission Edit',
+            'Permission Delete',
+
+            'Category List',
+            'Category Create',
+            'Category Edit',
+            'Category Delete',
+
+            'Article List',
+            'Article Create',
+            'Article Edit',
+            'Article Delete',
+
+            'Faq List',
+            'Faq Create',
+            'Faq Edit',
+            'Faq Delete',
+
+            'Team List',
+            'Team Create',
+            'Team Edit',
+            'Team Delete',
+
+            'Quiz List',
+            'Quiz Create',
+            'Quiz Edit',
+            'Quiz Delete',
+
+            'Quiz Form List',
+            'Quiz Form Create',
+            'Quiz Form Edit',
+            'Quiz Form Delete',
+
+            'Quiz Form Field List',
+            'Quiz Form Field Create',
+            'Quiz Form Field Edit',
+            'Quiz Form Field Delete',
+
+            'Page List',
+            'Page Create',
+            'Page Edit',
+            'Page Delete',
+
+        ];
+
+
+        foreach ($Permissions as $Permission) {
+
+            // Permission::Create(['name' => $Permission, 'slug' => Helper::slugify($Permission)]);
+            Permission::create(['name' => $Permission, 'slug' => Helper::slugify($Permission)]);
+
+        }
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
